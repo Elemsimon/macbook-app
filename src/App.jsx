e6fc5aa3f139c4e0435967b4a7accb6d838bdde5
Elemsimon <elemifesinachisimon@gmail.com>
2026-02-19 18:41:21 +0100

    product section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import ProductViewer from './components/productViewer'
 
 const App = () => {
     return (
         <main>
             <NavBar />
             <Hero />
+            <ProductViewer />
         </main>
     )
 }
