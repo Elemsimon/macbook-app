86a2e821a59e90a273fc53395a819261fa8d3f0a
Elemsimon <elemifesinachisimon@gmail.com>
2026-04-29 16:32:25 +0100

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Showcase from './components/Showcase'
 import Performance from './components/Performance'
 import Footer from './components/Footer'
 import Features from './components/Features'
+import Highlights from './components/Highlights'
 
 const App = () => {
     return (
@@ -16,7 +17,7 @@ const App = () => {
             <Showcase />
             <Performance />
             <Features />
-            <Highlight />
+            <Highlights />
             <Footer />
         </main>
     )
